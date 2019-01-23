52e0e4c485a2205b325a045d1a3f972234a2e63f
ansnoussi <anis0snoussi@gmail.com>
2019-01-23 19:04:14 +0000

    scripts

---
+++ morse/python.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...', 
+                    'C':'-.-.', 'D':'-..', 'E':'.', 
+                    'F':'..-.', 'G':'--.', 'H':'....', 
+                    'I':'..', 'J':'.---', 'K':'-.-', 
+                    'L':'.-..', 'M':'--', 'N':'-.', 
+                    'O':'---', 'P':'.--.', 'Q':'--.-', 
+                    'R':'.-.', 'S':'...', 'T':'-', 
+                    'U':'..-', 'V':'...-', 'W':'.--', 
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..', 
+                    '1':'.----', '2':'..---', '3':'...--', 
+                    '4':'....-', '5':'.....', '6':'-....', 
+                    '7':'--...', '8':'---..', '9':'----.', 
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-', 
+                    '?':'..--..', '/':'-..-.', '-':'-....-', 
+                    '(':'-.--.', ')':'-.--.-'} 
+
+
+def decrypt(message): 
+    message += ' '
+  
+    decipher = '' 
+    citext = '' 
+    for letter in message: 
+        if (letter != ' '): 
+            i = 0
+            citext += letter 
+        else: 
+            i += 1
+            if i == 2 : 
+                decipher += ' '
+            else: 
+                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT 
+                .values()).index(citext)] 
+                citext = '' 
+    return decipher 
+
+
+
+
+for i in range(1,1000):
+    s = "file/" + str(i) + ".txt"
+    f = open(s , 'r')
+    for l in f:
+        print(decrypt(l.strip()))
